d53a7c8c76f7140e7a3026bc2d7f6a32901b1cf2
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-01-08 21:53:26 +0100

    fix

--- script.js
+++ script.js
@@ -3,44 +3,43 @@ const numeriDiv = document.getElementById("numeriDiv")
 const sortNumeriDiv = document.getElementById("sortNumeriDiv")
 console.log(arrNumeri)
 
+let altezza = 20;
+let colorChange = 235;
+
 for (let i=0 ; i < arrNumeri.length; i++){
     numeriDiv.innerHTML += `<div id="numero-${arrNumeri[i]}">${arrNumeri[i]}</div>`
 }
 
 for (let i=0; i < arrNumeri.length; i++){
-    
-    for (let j = 0; j < arrNumeri.length; j++){
-        
-        let appoggio;
-        
-        if(arrNumeri[i] < arrNumeri[j]){   
-            appoggio = arrNumeri[i];
-            arrNumeri[i] = arrNumeri[j];
-            arrNumeri[j] = appoggio;
+
+        for (let j = 0; j < arrNumeri.length; j++){
+            if(arrNumeri[i] < arrNumeri[j]){   
+                appoggio = arrNumeri[i];
+                arrNumeri[i] = arrNumeri[j];
+                arrNumeri[j] = appoggio;
+            }
+            sortNumeriDiv.innerHTML = " ";
+            for (let i=0 ; i < arrNumeri.length; i++){
+                sortNumeriDiv.innerHTML += `<div id="sortedNumero-${arrNumeri[i]}">${arrNumeri[i]}</div>`;
+            }
         }
     }
+
+console.log(arrNumeri)
+
+for (let i=0 ; i < arrNumeri.length; i++){
+    altezza += 10;
+    colorChange -= 8;
+    let styleNum = document.getElementById("numero-"+(i+1));
+    styleNum.style.height=altezza.toString()+"px";
+    styleNum.style.backgroundColor = "rgb(235," + colorChange + ",52)";
+
+    let sortedNumero = document.getElementById("sortedNumero-"+(i+1));
+    sortedNumero.style.height=altezza.toString()+"px";
+    sortedNumero.style.backgroundColor = "rgb(235," + colorChange + ",52)";
+    
 }
 
-    console.log(arrNumeri)
-    
-    
-    for (let i=0 ; i < arrNumeri.length; i++){
-        sortNumeriDiv.innerHTML += `<div id="numeroSorted-${arrNumeri[i]}">${arrNumeri[i]}</div>`
-    }
-    
-    let altezza = 20;
-    let colorChange = 235;
-    
-    
-    for (let i=1 ; i < arrNumeri.length +1; i++){
-        altezza += 10;
-        colorChange -= 8;
-        let styleNum = document.getElementById("numero-"+(i));
-        styleNum.style.height=altezza.toString()+"px";
-        styleNum.style.backgroundColor = "rgb(235," + colorChange + ",52)";
 
-        let styleNumSorted = document.getElementById("numeroSorted-"+(i))
-        styleNumSorted.style.height=altezza.toString()+"px";
-        styleNumSorted.style.backgroundColor = "rgb(235," + colorChange + ",52)";
-    }
-    
+
+
